0c3b601e69ecf103a0ee4505699031c443d16ff0
Anton Anikin <aanikin@hot.ee>
2020-07-24 15:03:51 +0300

    added scripts needed for creating user

--- run_oracle.sh
+++ run_oracle.sh
@@ -8,3 +8,10 @@ docker run -d --name oracle_db \
 -e ORACLE_CHARACTERSET=AL32UTF8 \
 -v oracle-db:/opt/oracle/oradata \
 oracle/database:18.4.0-xe
+
+
+echo "alter session set \"_ORACLE_SCRIPT\"=true;"
+echo "CREATE USER SALDO000 IDENTIFIED BY SALDO000;"
+echo "GRANT CONNECT TO SALDO000;"
+echo "GRANT CONNECT, RESOURCE, DBA TO SALDO000;"
+echo "GRANT UNLIMITED TABLESPACE TO SALDO000;"

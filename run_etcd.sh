7e71a1919f03378d37ec2c13bbdfeeba52889a0b
Anton Anikin <aanikin@hot.ee>
2020-07-28 16:48:45 +0300

    added etcd version 2

--- run_etcd.sh
+++ run_etcd.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /tmp/etcd-data.tmp && mkdir -p /tmp/etcd-data.tmp && \
   chmod 700 /tmp/etcd-data.tmp && docker rmi gcr.io/etcd-development/etcd:v3.4.10 || true && \
-  docker run -d -p 2379:2379   -p 2380:2380 \
+  docker run -d -p 2379:2379  -e ETCD_ENABLE_V2=true -p 2380:2380 \
   --mount type=bind,source=/tmp/etcd-data.tmp,destination=/etcd-data \
     --name etcd-gcr-v3.4.10   gcr.io/etcd-development/etcd:v3.4.10  \
      /usr/local/bin/etcd   --name s1   --data-dir /etcd-data  \
